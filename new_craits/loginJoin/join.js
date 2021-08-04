cfd620ae96ae344639a6a97c74009957a81ae544
jameshan521 <69189932+jameshan521@users.noreply.github.com>
2021-08-04 16:18:43 +0900

    Delete new_craits/loginJoin directory

--- new_craits/loginJoin/join.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function toggle(source) {
-  checkboxes = document.getElementsByName("check");
-  for (var i = 0, n = checkboxes.length; i < n; i++) {
-    checkboxes[i].checked = source.checked;
-  }
-}
